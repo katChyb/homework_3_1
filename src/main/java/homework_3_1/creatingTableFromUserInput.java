13d11a85c2465e1161d6578c70aae6b3a3b425f8
katChyb <64010913+katChyb@users.noreply.github.com>
2022-08-08 13:12:29 +0200

    from 08/08 homework 3_1 numberToGuess v1 creatingTableFromUserInput v1

--- src/main/java/homework_3_1/creatingTableFromUserInput.java
+++ src/main/java/homework_3_1/creatingTableFromUserInput.java
@@ -16,7 +16,7 @@ public class creatingTableFromUserInput {
         System.out.println("Please provide numbers to populate the array");
 
        addingToTable(userSize,i,numbersTable);
-// do wyjasnienia wynik dla awerage i max
+
         System.out.println("Your table size is: " + userSize + "  Your table is: " + Arrays.toString(numbersTable) );
         System.out.println("Your table average is: " + Arrays.stream(numbersTable).average().getAsDouble());
         System.out.println("Your table Max. is: " + Arrays.stream(numbersTable).max().getAsInt());
